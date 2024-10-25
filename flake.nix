90c2ab123e446ff9a972d875d7f8c4e7e1c83469
Judah Sotomayor <jisotomayor@liberty.edu>
2024-10-25 12:00:00 -0400

    feat: Add flake infrastructure

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Enumeration Scripts";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+      {
+
+        devShells = forAllSystems ({ pkgs }: {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              bats
+            ];
+          };
+        });
+
+      };
+}
