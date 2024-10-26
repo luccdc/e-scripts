e90497c74e16cd9c1d0acbd43cfa8660644c0bb5
Judah Sotomayor <jisotomayor@liberty.edu>
2024-10-26 03:07:17 -0400

    fix: Correct bad tar creation
    Move xargs to use cp in order to tar just once
    Change shell to bash
    Ensure passwd only called for new users

--- linux/backup.sh
+++ linux/backup.sh
@@ -1,15 +1,17 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 [ -n "$TARBALLS" ] || read -p "Tarballs: " TARBALLS
 
 mkdir -p $(dirname ${TARBALLS})
 
-echo "Note that /etc and /var/lib are handled by default!"
+echo 'Note that /etc,' /var/{lib,www}, {/usr,}/lib/systemd 'are handled by default'
 [ -n "$BACKUP_TARGETS" ] || read -p "Extra files to backup: " BACKUP_TARGETS
 
-echo $TARBALLS | xargs -I '{}' -n 1 tar -cvzpf '{}' /etc /var/lib /var/www $BACKUP_TARGETS
+tar -czpf /tmp/i.tgz /etc /var/lib /var/www /lib/systemd /usr/lib/systemd $BACKUP_TARGETS
+echo $TARBALLS | xargs -n 1 cp /tmp/i.tgz &&
+    rm /tmp/i.tgz
 
 while read -p "Backup user to add: " BACKUP_USER; do
-    useradd -r -s /usr/bin/bash -G sudo ${BACKUP_USER}
-    passwd $BACKUP_USER
+    useradd -r -s /usr/bin/bash -G sudo ${BACKUP_USER} &&
+        passwd $BACKUP_USER
 done
