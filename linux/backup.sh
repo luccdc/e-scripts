7ff84734f2a70c5bce067ab3b953446632c9adb2
Judah Sotomayor <jisotomayor@liberty.edu>
2024-10-26 02:40:31 -0400

    feat(backup): Add backup script for linux

---
+++ linux/backup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+[ -z "$TARBALLS" ] || read -p "Tarballs: " TARBALLS
+
+mkdir -p $(dirname ${TARBALLS})
+
+echo "Note that /etc and /var/lib are handled by default!"
+[ -z "$BACKUP_TARGETS" ] || read -p "Extra files to backup: " BACKUP_TARGETS
+
+echo $TARBALLS | xargs -I '{}' -n 1 tar -cvzpf '{}' /etc /var/lib /var/www $BACKUP_TARGETS
+
+while read -p "Backup user to add: " BACKUP_USER; do
+    useradd -r -s /usr/bin/bash -G sudo ${BACKUP_USER}
+    passwd $BACKUP_USER
+done
