8811f11c13086cff497aed3baefdbeaa39e44152
Judah Sotomayor <jisotomayor@liberty.edu>
2025-02-06 12:19:13 -0500

    feat: Add centos repo fixer

---
+++ linux/fix-centos-yum.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+echo "This script will modify your repo list."
+read -p "Continue? (y/n): " cont
+
+[ "y" = "${cont,,}" ] &&
+    ( find /etc/yum.repos.d -type f -exec sed -i {} -r -e 's~^mirrorlist~#mirrorlist~' ;
+      find /etc/yum.repos.d -type f -exec sed -i {} -r -e 's~#?baseurl=http://mirror.centos~baseurl=http://vault.centos~' )
