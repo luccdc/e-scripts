2aab9dc2e504846b3c0343926e7301a7ef448f38
Judah Sotomayor <jisotomayor@liberty.edu>
2025-02-07 14:11:04 -0500

    fix: Correct semicolon in fix-centos.sh
    find -exec parameters require an ending semicolon, escaped to prevent
    shell interpretation.

--- linux/fix-centos-yum.sh
+++ linux/fix-centos-yum.sh
@@ -4,5 +4,5 @@ echo "This script will modify your repo list."
 read -p "Continue? (y/n): " cont
 
 [ "y" = "${cont,,}" ] &&
-    ( find /etc/yum.repos.d -type f -exec sed -i {} -r -e 's~^mirrorlist~#mirrorlist~' ;
-      find /etc/yum.repos.d -type f -exec sed -i {} -r -e 's~#?baseurl=http://mirror.centos~baseurl=http://vault.centos~' )
+    ( find /etc/yum.repos.d -type f -exec sed -i {} -r -e 's~^mirrorlist~#mirrorlist~' \;
+      find /etc/yum.repos.d -type f -exec sed -i {} -r -e 's~#?baseurl=http://mirror.centos~baseurl=http://vault.centos~' \; )
